cd5cebbeb297a2b1f271aa13eb09ffef3d976511
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-13 13:16:56 +0700

    add: create file ItemTodo.tsx

---
+++ src/Components/ItemTodo.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+interface Props {
+  
+}
+interface State {
+  
+}
+
+export default class ItemTodo extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
