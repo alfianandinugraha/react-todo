6d750a9ad8916e7a82c0806860b6e1919ce68676
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-13 20:22:34 +0700

    add: adding contact section on sidebar

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -4,11 +4,18 @@ export default class Sidebar extends Component {
   render() {
     return (
       <div>
-        <aside id="sidebar" className="container">
+        <aside id="sidebar" className="container sidebar">
           <div id="author">
-            <h3 className="author__title">About Author</h3>
+            <h3 className="sidebar__title">About Author</h3>
             <img src="/Images/alfian-andi.png" alt="" className="author__image"/>
             <p className="author__biograph">Hiii, I am Alfian Andi Nugraha. I am from Indonesia, I love coding and programming. Nice to meet you :). Looking for full-stack developer ? Hire me</p>
+          </div>
+          <div id="contact" className="contact">
+            <h3 className="sidebar__title">Contact me</h3>
+            <a
+              href="https://linktr.ee/alfianandinugraha"
+              className="contact__url"
+            >https://linktr.ee/alfianandinugraha</a>
           </div>
         </aside>
         <div className="backdrop"></div>
