8969f79863071047b07eecd978ca20aff6701ac0
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-13 13:02:21 +0700

    add: adding Header.tsx for header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+interface Props {
+  
+}
+interface State {
+  
+}
+
+export default class Header extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
