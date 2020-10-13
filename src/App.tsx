8e6f96de7382e9a3421f7642b1e9b6e5690463ff
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-13 13:27:15 +0700

    update: register todo and create loop five times

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Header from './Components/Header'
+import ItemTodo from './Components/ItemTodo'
 
 interface Props {
   
@@ -12,9 +13,14 @@ export default class App extends Component<Props, State> {
   state = {}
 
   render() {
+    const todoElementsTsx = [1, 2, 3, 4, 5].map((val: any, index: number) => <ItemTodo key={index}/>)
+
     return (
       <div>
         <Header />
+        <div id="todos">
+          {todoElementsTsx}
+        </div>
       </div>
     )
   }
