b281eb8c7ab050e585729c4545f2aab0d150c35f
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-13 20:12:59 +0700

    add: create element for author on sidebar

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -4,7 +4,14 @@ export default class Sidebar extends Component {
   render() {
     return (
       <div>
-        
+        <aside id="sidebar" className="container">
+          <div id="author">
+            <h3 className="author__title">About Author</h3>
+            <img src="/Images/alfian-andi.png" alt="" className="author__image"/>
+            <p className="author__biograph">Hiii, I am Alfian Andi Nugraha. I am from Indonesia, I love coding and programming. Nice to meet you :). Looking for full-stack developer ? Hire me</p>
+          </div>
+        </aside>
+        <div className="backdrop"></div>
       </div>
     )
   }
